c604f0a0fd2e390427fa6c2020d21bbc3428d277
PolarisEagle <90157103+PolarisEagle@users.noreply.github.com>
2022-09-07 19:04:28 -0700

    new

---
+++ DatabaseSearcher.py
@@ -0,0 +1,27 @@
+import random
+import time as t
+
+josh = "e"
+josad = "eas"
+'''
+Made by Miles..
+
+P7 Programming & Robotics.
+
+12/01/2021
+December 1st, 2021.
+
+'''
+
+print("connecting to database...")
+print("connected in... 214ms!")
+t.sleep(1)
+josh = int("insert phone number to lookup:")
+print("looking up phone number: ", josh)
+print("finding...")
+t.sleep('2')
+print("MATCH FOUND!")
+t.sleep('1')
+print("`100%` MATCH WITH `SARAH IONIS`.")
+print('found in 241ms.')
+josad = int(" Exit Program? Y or N?")
