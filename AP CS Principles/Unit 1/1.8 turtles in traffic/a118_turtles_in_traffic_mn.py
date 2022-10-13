93468af4d5415f1f9dd6e62b5b35786bb5e639c8
PolarisEagle <90157103+PolarisEagle@users.noreply.github.com>
2022-10-13 08:07:38 -0700

    Update a118_turtles_in_traffic_mn.py

--- AP CS Principles/Unit 1/1.8 turtles in traffic/a118_turtles_in_traffic_mn.py
+++ AP CS Principles/Unit 1/1.8 turtles in traffic/a118_turtles_in_traffic_mn.py
@@ -14,10 +14,10 @@ horiz_turtles = []
 vert_turtles = []
 
 # use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+#turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+turtle_shapes = ["circle", "turtle", "square"]
 horiz_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 vert_colors = ["darkred", "darkblue", "lime", "salmon", "indigo", "brown"]
-
 tloc = 50
 for s in turtle_shapes:
 
@@ -26,7 +26,7 @@ for s in turtle_shapes:
     ht.penup()
     new_color = horiz_colors.pop()
     ht.fillcolor(new_color)
-    ht.goto(-350, tloc)
+    ht.goto(-200, tloc)
     ht.setheading(0)
 
     vt = trtl.Turtle(shape=s)
@@ -34,7 +34,7 @@ for s in turtle_shapes:
     vt.penup()
     new_color = vert_colors.pop()
     vt.fillcolor(new_color)
-    vt.goto(-tloc, 350)
+    vt.goto(-tloc, 200)
     vt.setheading(270)
 
     tloc += 50
@@ -53,15 +53,24 @@ for step in range(200):
       hy = h.ycor()
       vx = v.xcor()    
       if (abs(hx - vx) > 30 and abs(hy - vy) > 30):
-        if hx < 250:
+        if hx < -20:
           h.forward(3)
-        v.forward(3)
+        else:
+          h.fillcolor('gray')
+        if vy > -0:
+          v.forward(3)
+        else:
+          v.fillcolor('gray')
+        print(vx,vy)
       else:
         if (abs(hx - vx) > 30):
           h.backward(3)
+          v.forward(3)
   print(abs(h.xcor() - v.xcor()))
-  if (abs(h.xcor() - v.xcor()) < 20):
+  if (abs(h.xcor() - v.xcor()) + abs(h.ycor() - v.ycor())< 20):
     print(abs(h.xcor() - v.xcor()) + abs(h.xcor() - v.ycor() < 20))
+    h.fillcolor('gray')
+    v.fillcolor('gray')
     print("removed", v)
     vert_turtles.remove(v)
     print("removed", h)
